01cebab46267c1643fd448442225ef1634fda91e
Subhash-Chamara <udayanga.subash@gmail.com>
2023-03-23 06:37:18 +0530

    View is Designed

--- src/main/java/lk/ijse/dep10/app/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/app/AppInitializer.java
@@ -1,8 +1,31 @@
 package lk.ijse.dep10.app;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lk.ijse.dep10.app.db.DBConnection;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static javafx.application.Application.launch;
+
+public class AppInitializer extends Application {
+
     public static void main(String[] args) {
 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(new FXMLLoader(getClass().getResource("/view/MainView.fxml")).load()));
+        primaryStage.centerOnScreen();
+        primaryStage.sizeToScene();
+        primaryStage.setMinWidth(900);
+        primaryStage.setMinHeight(600);
+        primaryStage.show();
     }
 
 }
---
+++ src/main/java/lk/ijse/dep10/app/controlls/MainViewController.java
@@ -0,0 +1,78 @@
+package lk.ijse.dep10.app.controlls;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
+public class MainViewController {
+
+    @FXML
+    private Button btnBrowse;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewStudent;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ImageView profilePicture;
+
+    @FXML
+    private TableView<?> tblStudents;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnBrowseOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblStudentOnKeyReleased(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
+}
